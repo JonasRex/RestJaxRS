64d25334c82652527b82cb59843a917c252e712b
JonasRex <78900583+JonasRex@users.noreply.github.com>
2022-03-01 21:45:31 +0100

    changed CorsFilter and POM

--- src/main/java/cors/CorsFilter.java
+++ src/main/java/cors/CorsFilter.java
@@ -6,6 +6,7 @@ import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
+
 @PreMatching
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
